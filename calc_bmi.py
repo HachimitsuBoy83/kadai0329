8c975c3adb694d89a0647105040f74038b05dce2
TomiyaShuta <s.tomiya.sys20@morijyobi.ac.jp>
2021-03-29 11:54:43 +0900

    BMI計算アプリの新規作成

---
+++ calc_bmi.py
@@ -0,0 +1,3 @@
+def get_area(kg,m):
+    area = kg / (m * m)
+    return area
---
+++ main.py
@@ -0,0 +1,6 @@
+import calc_bmi
+
+k = int(input("体重："))
+m = float(input("身長："))
+
+print(f"BMI：{calc_bmi.get_area(k,m)}")
